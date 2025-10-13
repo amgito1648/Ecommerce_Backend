d8445ba1b3158853fb8fa9203852483b07f1c2d7
amgito1648 <ccala25@unab.edu.co>
2025-10-13 16:00:18 -0500

    Migracion de images product

---
+++ Ecommerce_backend/database/migrations/2025_10_13_203329_create_images_product_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('images_product', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('product');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('images_product');
+    }
+};
