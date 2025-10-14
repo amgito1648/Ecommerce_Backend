14a8b64018db0cb5ce917c1ed91dcceb346f0746
amgito1648 <ccala25@unab.edu.co>
2025-10-13 20:14:01 -0500

    Category Factory

--- Ecommerce_backend/database/migrations/2025_10_13_193121_create_product_table.php
+++ Ecommerce_backend/database/migrations/2025_10_13_193121_create_product_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('url_image');
             $table->string('category');
             $table->timestamps();
+
+            $table->foreignId('category_id')->references('id')->on('categories');
         });
     }
 
