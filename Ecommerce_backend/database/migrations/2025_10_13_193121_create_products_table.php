305125221e72855d2ba30913e563b0aab66c1e2f
amgito1648 <ccala25@unab.edu.co>
2025-10-13 20:49:16 -0500

    ProductFactory

--- Ecommerce_backend/database/migrations/2025_10_13_193121_create_products_table.php
+++ Ecommerce_backend/database/migrations/2025_10_13_193121_create_products_table.php
@@ -9,13 +9,13 @@ return new class extends Migration
     
     public function up(): void
     {
-        Schema::create('product', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('description');
             $table->decimal('price');
             $table->string('url_image');
-            $table->string('category');
+            //$table->string('category');
             $table->timestamps();
 
             $table->foreignId('category_id')->references('id')->on('categories');
@@ -25,6 +25,6 @@ return new class extends Migration
     
     public function down(): void
     {
-        Schema::dropIfExists('product');
+        Schema::dropIfExists('products');
     }
 };
--- Ecommerce_backend/database/seeders/DatabaseSeeder.php
+++ Ecommerce_backend/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
+use Database\Factories\ProductFactory;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +17,8 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class
 
         ]);
+        Product::factory(1000)->create();
+
+        
     }
 }
