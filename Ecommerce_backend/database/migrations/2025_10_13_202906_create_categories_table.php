a775f5ac0080b0e043fa8347cd4049e4b68f6e0d
amgito1648 <ccala25@unab.edu.co>
2025-10-13 19:40:44 -0500

    CategorySeeder

--- Ecommerce_backend/database/migrations/2025_10_13_202906_create_categories_table.php
+++ Ecommerce_backend/database/migrations/2025_10_13_202906_create_categories_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     
     public function up(): void
     {
-        Schema::create('category', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
@@ -19,6 +19,6 @@ return new class extends Migration
     
     public function down(): void
     {
-        Schema::dropIfExists('category');
+        Schema::dropIfExists('categories');
     }
 };
