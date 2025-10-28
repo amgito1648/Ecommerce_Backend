a70a788170a4f8de1773407027bf1c1faea9779e
Ana Ramos <aramos598@unab.edu.co>
2025-10-28 14:43:54 -0500

    layouts admin

---
+++ Ecommerce_backend/resources/views/admin/categories/create.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layouts.app')
+
+
+@section('content')
+
+<h1> Add new Category</h1>
+@endsection
--- Ecommerce_backend/routes/web.php
+++ Ecommerce_backend/routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use GuzzleHttp\Promise\Create;
 use Illuminate\Support\Facades\Route;
 
 
-
+Route::get('/', [ProductController::class,'index']);
 
 Route::prefix('products')-> controller(ProductController::class)->group(function () {
     Route::get('/', 'index' );
@@ -16,4 +19,12 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/', [HomeController::class, 'welcome' ] );
+
+Route::prefix('admin')->controller(AdminController::class)->group(function(){
+
+    Route::get('/', [AdminController::class,'index'])->name('admin.index');
+    Route::get('/categories', [CategoryController::class,'create'])->name('admin.categories.create');
+
+});   
+
+
