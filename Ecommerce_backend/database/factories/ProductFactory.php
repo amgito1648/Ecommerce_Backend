6188c7702e3f2db6cfc0f97a6c31e93749de558c
amgito1648 <ccala25@unab.edu.co>
2025-11-15 18:45:51 -0500

    ajuste de los seeders

--- Ecommerce_backend/database/factories/ProductFactory.php
+++ Ecommerce_backend/database/factories/ProductFactory.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Factories;
+
+use App\Models\Brand;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -18,8 +20,9 @@ class ProductFactory extends Factory
             'name'=>fake()->name(),
             'description' =>fake() ->paragraph(),
             'price'=> fake()->randomFloat(2,10000,1000000),
-            'url_image' => fake()->imageUrl(640,480, 'products',true),
-            'category_id' =>Category::inRandomOrder()->first()->id
+            //'url_image' => fake()->imageUrl(640,480, 'products',true),
+            'category_id' =>Category::inRandomOrder()->first()->id,
+            'brand_id' =>Brand::inRandomOrder()->first()->id,
 
         ];
     }
