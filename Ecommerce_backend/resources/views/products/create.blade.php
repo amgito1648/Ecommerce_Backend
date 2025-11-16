92ed43c20dd6d6fe6bdf2e08c860fbc6deb17f11
amgito1648 <ccala25@unab.edu.co>
2025-11-15 21:54:27 -0500

    se agrego el store  de products

--- Ecommerce_backend/resources/views/products/create.blade.php
+++ Ecommerce_backend/resources/views/products/create.blade.php
@@ -5,7 +5,9 @@
 
     <div class="card">
         <div class="card-body">
-          <form>
+          <form action ="{{route('admin.products.store')}}" method="POST">
+            @csrf
+
       <!-- Nombre del producto -->
       <div class="input-group input-group-outline mb-3">
         <label for="productName" class="form-label">Product Name</label>
@@ -26,8 +28,8 @@
 
       <!-- Categoria del Producto -->
       <div class="input-group input-group-outline mb-3">
-        <select class= "form-control" id="productCategory">
-            <option value =""selected disabled>-- Category --</option>
+        <select class= "form-control" id="productCategory" name="category">
+            <option selected disabled>-- Category --</option>
              @foreach ( $categories as $item)
              <option value = "{{$item->id}}">{{$item->name}}</option>    
             @endforeach
@@ -38,8 +40,8 @@
 
       <!-- Marca del Producto -->
       <div class="input-group input-group-outline mb-3">
-        <select class= "form-control" id="productBrand">
-            <option value =""selected disabled>-- Brand --</option>
+        <select class= "form-control" id="productBrand" name= "brand">
+            <option selected disabled>-- Brand --</option>
             @foreach ($brands as $item )
                 <option value="{{$item->id}}">{{$item->name}}</option>             
             @endforeach
